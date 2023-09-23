3766a32247f895ec790c148775fee933792d517d
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-23 11:06:11 -0500

    intro curved bar

---
+++ lib/utils/tab_map.dart
@@ -0,0 +1,11 @@
+import 'package:recommendationsapp/pages/add_page.dart';
+import 'package:recommendationsapp/pages/list_page.dart';
+
+final List<Map<String, dynamic>> pageDetails = [
+  {
+    'pageName': ListPage(),
+  },
+  {
+    'pageName': AddPage(),
+  }
+];
