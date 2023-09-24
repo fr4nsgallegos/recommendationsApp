b2b6b03e47a868e518aca470a84e424427a8085b
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-09-24 11:35:17 -0500

    map page wiith markers

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +10,58 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  CollectionReference recommendationReference =
+      FirebaseFirestore.instance.collection('recommendations');
+
+  Set<Marker> myMarkers = {};
+
+  Future<void> getRecommendations() async {
+    await recommendationReference.get().then((value) {
+      QuerySnapshot recommendationsCollection = value;
+      List<QueryDocumentSnapshot> docs = recommendationsCollection.docs;
+
+      docs.forEach(
+        (element) {
+          Map<String, dynamic> myDoc = element.data() as Map<String, dynamic>;
+          myDoc['id'] = element.id;
+          // print(myDoc);
+          // print("---------------");
+          // print(element['lat']);
+          // print(element['lang']);
+          double latAux = element['lat'];
+          double langAux = element['lang'];
+          Marker marker = Marker(
+            markerId: MarkerId(
+              myMarkers.length.toString(),
+            ),
+            position: LatLng(latAux, langAux),
+          );
+          myMarkers.add(marker);
+        },
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    getRecommendations();
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ubicaciones"),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-10.662173, -75.749106),
+          zoom: 6,
+        ),
+        markers: myMarkers,
+      ),
+    );
   }
 }
